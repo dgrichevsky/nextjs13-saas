9a41e630a2620eea2d5dded372246fe262a05772
Dan Grichevsky <dgrichevsky@gmail.com>
2023-02-02 15:15:22 -0500

    eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Home from '../app/page';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Home />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
